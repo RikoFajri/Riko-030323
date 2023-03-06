c3d91abd04fc24d4c4931af9b8a87d98b0248661
RikoFajri <126840218+RikoFajri@users.noreply.github.com>
2023-03-06 13:29:51 +0700

    Memasukin 3 File (ArimatikaDemo.java),(OutputVariable.java),(VariableSamples.java)

---
+++ VariableSamples.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Riko030323;
+
+/**
+ *
+ * @author HP
+ */
+public class VariableSamples {
+    public static void Main ( String [] args ){
+           /// deklarasi a tipe data dengan nama variable
+           /// result dan tipe data boolean
+           boolean  result;
+           
+           /// deklarasi tipe data dengan nama variabel
+           /// option dan tipe data char 
+           char option;
+           option = 'c';  /// menandai'c' sebagai option 
+           
+           /// deklarasi tipe data dengan nama variabel
+           /// grade, double tipe data dan telah di inisialisasi
+           /// to 0.0
+           double grade = 0.0;
+    }
+}
